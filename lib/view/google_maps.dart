5b087ffe85f4fe413fad60153e818f5c25105a5a
somer0801 <somer0801@hmail.com>
2024-02-27 00:00:57 +0500

    Initial commit

---
+++ lib/view/google_maps.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GoogleMapsScreen extends StatefulWidget {
+  const GoogleMapsScreen({super.key});
+
+  @override
+  State<GoogleMapsScreen> createState() => _GoogleMapsScreenState();
+}
+
+class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
